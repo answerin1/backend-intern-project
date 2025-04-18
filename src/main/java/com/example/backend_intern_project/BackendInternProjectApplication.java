c0f861ea35c1667341be0cbeacbb2321bc2d6df6
answerin1 <dabin990723@naver.com>
2025-04-18 11:06:44 +0900

    initial commit

---
+++ src/main/java/com/example/backend_intern_project/BackendInternProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_intern_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInternProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInternProjectApplication.class, args);
+	}
+
+}
