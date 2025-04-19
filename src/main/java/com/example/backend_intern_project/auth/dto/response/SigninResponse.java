bc0ce51f9e4d7b22392e262dca22223e19c195ec
answerin1 <dabin990723@naver.com>
2025-04-19 17:13:02 +0900

    feat: Add Response DTO

---
+++ src/main/java/com/example/backend_intern_project/auth/dto/response/SigninResponse.java
@@ -0,0 +1,13 @@
+package com.example.backend_intern_project.auth.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class SigninResponse {
+
+    private final String bearerToken;
+
+    public SigninResponse(String bearerToken) {
+        this.bearerToken = bearerToken;
+    }
+}
---
+++ src/main/java/com/example/backend_intern_project/auth/dto/response/SignupResponse.java
@@ -0,0 +1,13 @@
+package com.example.backend_intern_project.auth.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class SignupResponse {
+
+    private final String bearerToken;
+
+    public SignupResponse(String bearerToken) {
+        this.bearerToken = bearerToken;
+    }
+}
