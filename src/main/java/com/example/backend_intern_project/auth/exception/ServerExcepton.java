bc6b46e4795c647f00cfca48fd213ec3340b4778
answerin1 <dabin990723@naver.com>
2025-04-19 17:12:14 +0900

    feat: Add ServerException for internal errors

---
+++ src/main/java/com/example/backend_intern_project/auth/exception/ServerExcepton.java
@@ -0,0 +1,8 @@
+package com.example.backend_intern_project.auth.exception;
+
+public class ServerExcepton extends RuntimeException {
+
+    public ServerExcepton(String message) {
+        super(message);
+    }
+}
