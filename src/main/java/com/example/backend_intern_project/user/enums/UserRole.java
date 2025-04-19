5d79060369f6000afa6a8273afd9a969423c173c
answerin1 <dabin990723@naver.com>
2025-04-19 17:17:01 +0900

    feat: Define UserRole enum for role-based access

---
+++ src/main/java/com/example/backend_intern_project/user/enums/UserRole.java
@@ -0,0 +1,15 @@
+package com.example.backend_intern_project.user.enums;
+
+import com.example.backend_intern_project.auth.exception.InvalidRequestException;
+import java.util.Arrays;
+
+public enum UserRole {
+    ADMIN, USER;
+
+    public static UserRole of(String role) {
+        return Arrays.stream(UserRole.values())
+            .filter(r -> r.name().equalsIgnoreCase(role))
+            .findFirst()
+            .orElseThrow(() -> new InvalidRequestException("유효하지 않은 UserRole"));
+    }
+}
