1bbcfeaf2ba32b11a6a6b15a276f1e42fa2edaa4
answerin1 <dabin990723@naver.com>
2025-04-20 04:29:24 +0900

    fix: Handle signup/signin exceptions more gracefully

---
+++ src/main/java/com/example/backend_intern_project/auth/exception/SigninException.java
@@ -0,0 +1,7 @@
+package com.example.backend_intern_project.auth.exception;
+
+public class SigninException extends RuntimeException {
+    public SigninException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/backend_intern_project/auth/exception/SignupException.java
@@ -0,0 +1,8 @@
+package com.example.backend_intern_project.auth.exception;
+
+public class SignupException extends RuntimeException {
+    public SignupException(String message) {
+        super(message);
+    }
+}
+
