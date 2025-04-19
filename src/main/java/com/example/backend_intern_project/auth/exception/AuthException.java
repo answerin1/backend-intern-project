f6a53e583eb6e6b3064555b12553ac100a4e283f
answerin1 <dabin990723@naver.com>
2025-04-19 17:08:40 +0900

    feat: Create custom AuthException for login failures

---
+++ src/main/java/com/example/backend_intern_project/auth/exception/AuthException.java
@@ -0,0 +1,8 @@
+package com.example.backend_intern_project.auth.exception;
+
+public class AuthException extends RuntimeException {
+
+    public AuthException(String message) {
+        super(message);
+    }
+}
