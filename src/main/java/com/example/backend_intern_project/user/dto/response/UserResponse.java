ff4d7eafe563e7d98417a231cf5bc8b8e55b108f
answerin1 <dabin990723@naver.com>
2025-04-20 04:32:31 +0900

    refactor: Update UserResponse structure for consistent API responses

--- src/main/java/com/example/backend_intern_project/user/dto/response/UserResponse.java
+++ src/main/java/com/example/backend_intern_project/user/dto/response/UserResponse.java
@@ -2,14 +2,29 @@ package com.example.backend_intern_project.user.dto.response;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class UserResponse {
+    private final Long userId;
+    private final String username;
+    private final String nickname;
+    private final RoleResponse roles;
 
-    private final Long id;
-    private final String email;
 
-    public UserResponse(Long id, String email) {
-        this.id = id;
-        this.email = email;
+    public UserResponse(Long userId, String username, String nickname, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.nickname = nickname;
+        this.roles = new RoleResponse(role);
+    }
+
+    @Getter
+    public static class RoleResponse {
+        private final String role;
+
+        public RoleResponse(String role) {
+            this.role = role;
+        }
     }
 }
