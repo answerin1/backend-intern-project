70b24f46df10512d5a57dab01b93284197670a5a
answerin1 <dabin990723@naver.com>
2025-04-20 04:30:29 +0900

    fix: delete unused code

--- src/main/java/com/example/backend_intern_project/auth/exception/AuthException.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend_intern_project.auth.exception;
-
-public class AuthException extends RuntimeException {
-
-    public AuthException(String message) {
-        super(message);
-    }
-}
