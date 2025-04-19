53e8a82d3f2bcef6f98736aeb2608b26bbff4e46
answerin1 <dabin990723@naver.com>
2025-04-19 17:14:00 +0900

    feat: Define User entity with role and password

---
+++ src/main/java/com/example/backend_intern_project/user/entity/User.java
@@ -0,0 +1,47 @@
+package com.example.backend_intern_project.user.entity;
+
+import com.example.backend_intern_project.auth.common.AuthUser;
+import com.example.backend_intern_project.auth.entity.Timestamped;
+import com.example.backend_intern_project.user.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "`user`")
+public class User extends Timestamped {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    public User(String email, String password, UserRole userRole) {
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+    }
+
+    private User(Long id, String email, UserRole userRole) {
+        this.id = id;
+        this.email = email;
+        this.userRole = userRole;
+    }
+
+    public static User fromAuthUser(AuthUser authUser) {
+        return new User(authUser.getId(), authUser.getEmail(), authUser.getUserRole());
+    }
+
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public void updateRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+}
