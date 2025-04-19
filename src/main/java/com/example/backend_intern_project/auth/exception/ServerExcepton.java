7a697ca38a7c2459f3e0ebb44d23d63a7ed79761
answerin1 <dabin990723@naver.com>
2025-04-20 04:30:11 +0900

    fix: delete unused code

--- src/main/java/com/example/backend_intern_project/auth/exception/ServerExcepton.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend_intern_project.auth.exception;
-
-public class ServerExcepton extends RuntimeException {
-
-    public ServerExcepton(String message) {
-        super(message);
-    }
-}
