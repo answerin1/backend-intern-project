11ea113ec210b24b7c0a93b4aae55b13f47e80da
answerin1 <dabin990723@naver.com>
2025-04-20 04:28:59 +0900

    fix: Update AuthController to handle user authentication correctly

--- src/main/java/com/example/backend_intern_project/auth/controller/AuthController.java
+++ src/main/java/com/example/backend_intern_project/auth/controller/AuthController.java
@@ -2,28 +2,25 @@ package com.example.backend_intern_project.auth.controller;
 
 import com.example.backend_intern_project.auth.dto.request.SigninRequest;
 import com.example.backend_intern_project.auth.dto.request.SignupRequest;
-import com.example.backend_intern_project.auth.dto.response.SigninResponse;
-import com.example.backend_intern_project.auth.dto.response.SignupResponse;
 import com.example.backend_intern_project.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
+        private final AuthService authService;
 
-    @PostMapping("/auth/signup")
-    public SignupResponse signup(@Valid @RequestBody SignupRequest signupRequest) {
-        return authService.signup(signupRequest);
-    }
+        @PostMapping("/signup")
+        public ResponseEntity<?> signup(@Valid @RequestBody SignupRequest request) {
+            return ResponseEntity.ok(authService.signup(request));
+        }
 
-    @PostMapping("/auth/signin")
-    public SigninResponse signin(@Valid @RequestBody SigninRequest signinRequest) {
-        return authService.signin(signinRequest);
+        @PostMapping("/signin")
+        public ResponseEntity<?> signin(@Valid @RequestBody SigninRequest request) {
+            return ResponseEntity.ok(authService.signin(request));
+        }
     }
-}
