51bbdcdb632ea7018b202446471bf5a1f89ecc4c
answerin1 <dabin990723@naver.com>
2025-04-20 04:52:25 +0900

    fix: Delete unused code

--- src/main/java/com/example/backend_intern_project/auth/exception/SigninException.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend_intern_project.auth.exception;
-
-public class SigninException extends RuntimeException {
-    public SigninException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/backend_intern_project/auth/exception/SignupException.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend_intern_project.auth.exception;
-
-public class SignupException extends RuntimeException {
-    public SignupException(String message) {
-        super(message);
-    }
-}
-
